7799ee8abf23c4439d89c5531ab551c0f09b4f80
Khan Mohd Asif Altaf <161149248+asifkhan7060@users.noreply.github.com>
2026-07-14 08:14:05 +0530

    Clean up moveZeroes method by removing comments
    Removed commented-out code that caused time exceed issues.

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,6 +1,23 @@
 class Solution {
     public void moveZeroes(int[] nums) {
 
+        // My logic - code runs but time exceed problem :
+        
+        // int store = 0;
+
+        // for (int i = 0; i < nums.length; i++) {
+
+        //     if (nums[i] == 0) {
+
+        //         for (int j = i; j < nums.length - 1; j++) {
+        //             nums[j] = nums[j + 1];
+        //         }
+
+        //         store = nums.length - 1;
+        //         nums[store] = 0;
+        //     }
+        // }
+
         int store = 0;
 
         for (int i = 0; i < nums.length; i++) {
